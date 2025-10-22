c1703bd70c19436ee68d96b09d8790b80debf760
anapaulafo <foanapaula@gmail.com>
2025-10-21 21:05:54 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import { Home } from './pages/Home'
+import { Header } from './components/Header/Header'
+export const App = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Header />
+    <Home />
+    </>
+  )
+}
+
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import './Header.css'
+export const Header = () => {
+  return (
+    <div className="header">
+      <h1 className="header-title">Header da página</h1>
+    </div>
+  )
+}
+
